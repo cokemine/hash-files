24475120bb4e4db099a3775afd27ab68ff9c8803
cokemine <aptx4561@gmail.com>
2022-02-13 14:50:31 +0800

    feat: add log

--- cmd/hashfiles/hashfiles.go
+++ cmd/hashfiles/hashfiles.go
@@ -4,11 +4,13 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"sync"
+	"time"
 
 	hashfiles "github.com/cokemine/hash-files"
 )
@@ -17,6 +19,7 @@ var (
 	dirPath     = flag.String("dir", "", "The directory to be hashed")
 	algo        = flag.String("algo", "md5", "The hash algorithm to use, multiple algorithms can be specified by comma separated")
 	parallelNum = flag.Int("parallel", runtime.NumCPU(), "The number of parallel workers")
+	verbose     = flag.Bool("verbose", false, "Verbose output log")
 )
 
 func main() {
@@ -30,14 +33,16 @@ func main() {
 
 	files, err := hashfiles.ListDir(*dirPath)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	var timeConsumed int64
 
 	for _, algo := range strings.Split(*algo, ",") {
 		var hashFn func(filePath string) (string, error)
 		var wg sync.WaitGroup
+		algo := strings.TrimSpace(algo)
 
-		switch algo := strings.TrimSpace(algo); algo {
+		switch algo {
 		case "md5":
 			hashFn = hashfiles.GetMD5
 		case "sha1":
@@ -47,11 +52,13 @@ func main() {
 		case "quickxorhash":
 			hashFn = hashfiles.GetQuickXORHash
 		default:
-			panic(errors.New("unsupported algorithm"))
+			log.Fatal(errors.New("unsupported algorithm"))
 		}
 
 		result := make([]string, len(files))
 		fileWorkers := SliceChunk(files, *parallelNum)
+
+		timeStart := time.Now().Unix()
 
 		for i := range fileWorkers {
 			l := len(fileWorkers[i])
@@ -59,18 +66,35 @@ func main() {
 			for j := range fileWorkers[i] {
 				go func(j int, file string) {
 					defer wg.Done()
+
 					encoded, err := hashFn(file)
 					if err != nil {
 						panic(err)
 					}
-					result[i*l+j] = fmt.Sprintf("%s %s\n", encoded, hashfiles.TransformPath(strings.TrimPrefix(file, *dirPath)))
+
+					fileName := hashfiles.TransformPath(strings.TrimPrefix(file, *dirPath))
+					result[i*l+j] = fmt.Sprintf("%s %s\n", encoded, fileName)
+
+					if *verbose {
+						fmt.Printf("%s [%d,%d]: %s, result: %s\n", algo, i*l+j+1, len(files), fileName, encoded)
+					}
+
 				}(j, fileWorkers[i][j])
 			}
 			wg.Wait()
 		}
 
-		hashfiles.WriteFile(filepath.Join(*dirPath, fmt.Sprintf("%ssum.txt", algo)), strings.Join(result, "\n"))
+		timeEnd := time.Now().Unix()
+		timeConsumed += timeEnd - timeStart
+		log.Printf("%s: %d files, %d seconds\n", algo, len(files), timeEnd-timeStart)
+
+		err := hashfiles.WriteFile(filepath.Join(*dirPath, fmt.Sprintf("%ssum.txt", algo)), strings.Join(result, "\n"))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
+	log.Printf("All files hashed, %d seconds\n", timeConsumed)
 }
 
 func SliceChunk(s []string, size int) [][]string {
