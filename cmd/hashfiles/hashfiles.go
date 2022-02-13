6f6b8b86a2c13042841c24773f54406eae1ef4ce
cokemine <aptx4561@gmail.com>
2022-02-13 14:17:58 +0800

    feat: support multiple algorithms

--- cmd/hashfiles/hashfiles.go
+++ cmd/hashfiles/hashfiles.go
@@ -15,7 +15,7 @@ import (
 
 var (
 	dirPath     = flag.String("dir", "", "The directory to be hashed")
-	algo        = flag.String("algo", "md5", "The hash algorithm to use")
+	algo        = flag.String("algo", "md5", "The hash algorithm to use, multiple algorithms can be specified by comma separated")
 	parallelNum = flag.Int("parallel", runtime.NumCPU(), "The number of parallel workers")
 )
 
@@ -33,42 +33,44 @@ func main() {
 		panic(err)
 	}
 
-	var hashFn func(filePath string) (string, error)
-	var wg sync.WaitGroup
+	for _, algo := range strings.Split(*algo, ",") {
+		var hashFn func(filePath string) (string, error)
+		var wg sync.WaitGroup
 
-	switch *algo {
-	case "md5":
-		hashFn = hashfiles.GetMD5
-	case "sha1":
-		hashFn = hashfiles.GetSHA1
-	case "sha256":
-		hashFn = hashfiles.GetSHA256
-	case "quickxorhash":
-		hashFn = hashfiles.GetQuickXORHash
-	default:
-		panic(errors.New("unsupported algorithm"))
-	}
-
-	result := make([]string, len(files))
-	fileWorkers := SliceChunk(files, *parallelNum)
-
-	for i := range fileWorkers {
-		l := len(fileWorkers[i])
-		wg.Add(l)
-		for j := range fileWorkers[i] {
-			go func(j int, file string) {
-				defer wg.Done()
-				encoded, err := hashFn(file)
-				if err != nil {
-					panic(err)
-				}
-				result[i*l+j] = fmt.Sprintf("%s %s\n", encoded, hashfiles.TransformPath(strings.TrimPrefix(file, *dirPath)))
-			}(j, fileWorkers[i][j])
+		switch algo := strings.TrimSpace(algo); algo {
+		case "md5":
+			hashFn = hashfiles.GetMD5
+		case "sha1":
+			hashFn = hashfiles.GetSHA1
+		case "sha256":
+			hashFn = hashfiles.GetSHA256
+		case "quickxorhash":
+			hashFn = hashfiles.GetQuickXORHash
+		default:
+			panic(errors.New("unsupported algorithm"))
 		}
-		wg.Wait()
-	}
 
-	hashfiles.WriteFile(filepath.Join(*dirPath, "checksums.txt"), strings.Join(result, "\n"))
+		result := make([]string, len(files))
+		fileWorkers := SliceChunk(files, *parallelNum)
+
+		for i := range fileWorkers {
+			l := len(fileWorkers[i])
+			wg.Add(l)
+			for j := range fileWorkers[i] {
+				go func(j int, file string) {
+					defer wg.Done()
+					encoded, err := hashFn(file)
+					if err != nil {
+						panic(err)
+					}
+					result[i*l+j] = fmt.Sprintf("%s %s\n", encoded, hashfiles.TransformPath(strings.TrimPrefix(file, *dirPath)))
+				}(j, fileWorkers[i][j])
+			}
+			wg.Wait()
+		}
+
+		hashfiles.WriteFile(filepath.Join(*dirPath, fmt.Sprintf("%ssum.txt", algo)), strings.Join(result, "\n"))
+	}
 }
 
 func SliceChunk(s []string, size int) [][]string {
