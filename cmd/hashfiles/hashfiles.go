59cb81ab01d265a3a31a4e29d2989b375f408f9d
cokemine <aptx4561@gmail.com>
2022-03-30 15:50:18 +0800

    feat: add verify method

--- cmd/hashfiles/hashfiles.go
+++ cmd/hashfiles/hashfiles.go
@@ -1,99 +1,202 @@
 package main
 
 import (
-	"errors"
-	"flag"
+	"bufio"
 	"fmt"
+	"github.com/cokemine/hashfiles/pkg/hashfiles"
+	"github.com/urfave/cli/v2"
+	"io"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/cokemine/hashfiles"
 )
 
 type Empty struct{}
 
-var (
-	pwd, _      = os.Getwd()
-	dirPath     = flag.String("dir", pwd, "The directory to be hashed")
-	algo        = flag.String("algo", "md5", "The hash algorithm to use, multiple algorithms can be specified by comma separated")
-	parallelNum = flag.Int("parallel", runtime.NumCPU(), "The number of parallel workers")
-	verbose     = flag.Bool("verbose", false, "Verbose output log")
-)
+func getHashFn(algo string) func(filePath string) (string, error) {
+	switch algo {
+	case "md5":
+		return hashfiles.GetMD5
+	case "sha1":
+		return hashfiles.GetSHA1
+	case "sha256":
+		return hashfiles.GetSHA256
+	case "quickxorhash":
+		return hashfiles.GetQuickXORHash
+	default:
+		log.Fatalf("unsupported algorithm: %s", algo)
+	}
+	return nil
+}
 
 func main() {
-	flag.Parse()
+	pwd, _ := os.Getwd()
 
-	*dirPath = strings.TrimSuffix(*dirPath, string(os.PathSeparator)) + string(os.PathSeparator)
-	*algo = strings.ToLower(*algo)
+	var (
+		algo        string
+		dirPath     string
+		parallelNum int
+		verbose     bool
+	)
 
-	files, err := hashfiles.ListDir(*dirPath)
-	if err != nil {
-		log.Fatal(err)
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:        "dir",
+			Aliases:     []string{"d"},
+			Value:       pwd,
+			Usage:       "The directory to be hashed",
+			Destination: &dirPath,
+		},
+		&cli.StringFlag{
+			Name:        "algo",
+			Aliases:     []string{"a"},
+			Value:       "md5",
+			Usage:       "The hash algorithm to use, multiple algorithms can be specified by comma separated",
+			Destination: &algo,
+		},
+		&cli.IntFlag{
+			Name:        "parallel",
+			Aliases:     []string{"n"},
+			Value:       runtime.NumCPU(),
+			Usage:       "The number of parallel workers",
+			Destination: &parallelNum,
+		},
+		&cli.BoolFlag{
+			Name:        "verbose",
+			Value:       false,
+			Usage:       "Verbose output log",
+			Destination: &verbose,
+		},
 	}
 
 	var timeConsumed int64
+	timeStart := time.Now().Unix()
 
-	for _, algo := range strings.Split(*algo, ",") {
-		var hashFn func(filePath string) (string, error)
-		algo := strings.TrimSpace(algo)
+	app := &cli.App{
+		Name:  "hashfiles",
+		Usage: "Recursively generate checksum of all files in a directory",
+		Flags: flags,
+		Commands: []*cli.Command{
+			{
+				Name:  "hash",
+				Usage: "Hash files",
+				Flags: flags,
+				Action: func(c *cli.Context) error {
+					dirPath = strings.TrimSuffix(dirPath, string(os.PathSeparator)) + string(os.PathSeparator)
+					algo = strings.ToLower(algo)
+					files, err := hashfiles.ListDir(dirPath)
+					if err != nil {
+						log.Fatal(err)
+					}
 
-		switch algo {
-		case "md5":
-			hashFn = hashfiles.GetMD5
-		case "sha1":
-			hashFn = hashfiles.GetSHA1
-		case "sha256":
-			hashFn = hashfiles.GetSHA256
-		case "quickxorhash":
-			hashFn = hashfiles.GetQuickXORHash
-		default:
-			log.Fatal(errors.New("unsupported algorithm"))
-		}
+					for _, algo := range strings.Split(algo, ",") {
+						algo := strings.TrimSpace(algo)
 
-		filesChan, wg := make(chan Empty, *parallelNum), sync.WaitGroup{}
-		result := make([]string, len(files))
-		timeStart := time.Now().Unix()
+						filesChan, wg := make(chan Empty, parallelNum), sync.WaitGroup{}
+						result := make([]string, len(files))
 
-		for i := range files {
-			wg.Add(1)
-			filesChan <- Empty{}
+						hashFn := getHashFn(algo)
 
-			go func(i int, file string) {
-				defer func() {
-					wg.Done()
-					<-filesChan
-				}()
+						for i := range files {
+							wg.Add(1)
+							filesChan <- Empty{}
 
-				encoded, err := hashFn(file)
-				if err != nil {
-					panic(err)
-				}
+							go func(i int, file string) {
+								defer func() {
+									wg.Done()
+									<-filesChan
+								}()
 
-				fileName := hashfiles.TransformPath(strings.TrimPrefix(file, *dirPath))
-				result[i] = fmt.Sprintf("%s %s\n", encoded, fileName)
+								encoded, err := hashFn(file)
+								if err != nil {
+									panic(err)
+								}
 
-				if *verbose {
-					fmt.Printf("%s [%d,%d]: %s, result: %s\n", algo, i+1, len(files), fileName, encoded)
-				}
-			}(i, files[i])
-		}
+								fileName := hashfiles.TransformPath(strings.TrimPrefix(file, dirPath))
+								result[i] = fmt.Sprintf("%s %s\n", encoded, fileName)
 
-		wg.Wait()
+								if verbose {
+									log.Printf("%s [%d,%d]: %s, result: %s\n", algo, i+1, len(files), fileName, encoded)
+								}
+							}(i, files[i])
+						}
 
-		timeEnd := time.Now().Unix()
-		timeConsumed += timeEnd - timeStart
-		log.Printf("%s: %d files, %d seconds\n", algo, len(files), timeEnd-timeStart)
+						wg.Wait()
 
-		err := hashfiles.WriteFile(filepath.Join(*dirPath, fmt.Sprintf("%ssum.txt", algo)), strings.Join(result, "\n"))
-		if err != nil {
-			log.Fatal(err)
-		}
+						timeEnd := time.Now().Unix()
+						timeConsumed += timeEnd - timeStart
+						log.Printf("%s: %d files, %d seconds\n", algo, len(files), timeEnd-timeStart)
+
+						err := hashfiles.WriteFile(filepath.Join(dirPath, fmt.Sprintf("%ssum.txt", algo)), strings.Join(result, "\n"))
+						if err != nil {
+							log.Fatal(err)
+						}
+						log.Printf("All files hashed, %d seconds\n", timeConsumed)
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "verify",
+				Usage: "Verify files",
+				Flags: flags,
+				Action: func(c *cli.Context) error {
+					dirPath = strings.TrimSuffix(dirPath, string(os.PathSeparator)) + string(os.PathSeparator)
+					algo = strings.ToLower(algo)
+
+					for _, algo := range strings.Split(algo, ",") {
+						algo := strings.TrimSpace(algo)
+						hashFn := getHashFn(algo)
+						sumFile := path.Join(dirPath, fmt.Sprintf("%ssum.txt", algo))
+						file, err := os.Open(sumFile)
+						if err != nil {
+							log.Fatal(err)
+						}
+						r := bufio.NewReader(file)
+						i, tot, unMatched := 0, 0, 0
+						for {
+							line, err := r.ReadString('\n')
+							line = strings.TrimSpace(line)
+							if err == io.EOF {
+								break
+							} else if err != nil {
+								log.Fatal(err)
+							}
+							if line == "" {
+								continue
+							}
+							i = i + 1
+							sum := strings.SplitN(line, " ", 2)
+							fileHash, err := hashFn(path.Join(dirPath, sum[1]))
+							if err != nil {
+								log.Fatal(err)
+							}
+							if fileHash != sum[0] {
+								unMatched++
+								log.Printf("[Not Matched] %s [%d]: %s\n", algo, i, sum[1])
+							} else if verbose {
+								log.Printf("[Matched] %s [%d]: %s\n", algo, i, sum[1])
+							}
+							tot++
+						}
+						timeEnd := time.Now().Unix()
+						timeConsumed += timeEnd - timeStart
+						log.Printf("%s: %d files, %d seconds, UnMatched count: %d\n", algo, tot, timeEnd-timeStart, unMatched)
+					}
+					log.Printf("All files Verified, %d seconds\n", timeConsumed)
+					return nil
+				},
+			},
+		},
 	}
 
-	log.Printf("All files hashed, %d seconds\n", timeConsumed)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/cokemine/hashfiles
 
 go 1.17
 
-require github.com/rclone/rclone v1.57.0
+require (
+	github.com/rclone/rclone v1.57.0
+	github.com/urfave/cli/v2 v2.4.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+)
